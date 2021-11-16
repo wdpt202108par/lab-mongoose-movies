67fd9fcea3103c88884373f0bb31cd64a94cf198
aude.claquesin <aude.claquesin@gmail.com>
2021-11-16 21:15:21 +0100

    Iteration 0

---
+++ bin/seeds.js
@@ -0,0 +1,17 @@
+const celebrityData = [
+    celebrity1 = {
+        name : "",
+        occupation: "",
+        catchPhrase: ""
+    },
+    celebrity2 = {
+        name : "",
+        occupation: "",
+        catchPhrase: ""
+    },
+    celebrity3 = {
+        name : "",
+        occupation: "",
+        catchPhrase: ""
+    },
+] 
