d886cd4497e542cab85f663ac47de2f588e2c58a
Yooyoo56 <tak.yoojeong@gmail.com>
2021-11-17 11:00:36 +0100

    iteration 2 finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const celebrity = require('../models/celebrity');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET celebrities page */
+//iteration 2 
+router.get('/celebrities', (req, res, next) =>{
+  celebrity.find()
+  .then(function (celebrityDB){
+    res.render('celebrities/index', {myCeleb : celebrityDB});
+    console.log('Connected to the port 3000');
+  })
+  .catch(function (err){
+    console.log('Error! Durint open the celebrtiy page');
+  })
+})
 
 module.exports = router;
