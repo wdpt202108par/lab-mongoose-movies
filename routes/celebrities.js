eb1ce57c849620ce9317b2131448b94a8f02278b
Cindy <cindy.confiant@gmail.com>
2021-11-18 19:58:47 +0100

    iteration 6 in progress

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Celebrity = require('../models/Celebrity.model.js')
 
+// ITERATION 2 : Listing our celebrities
 router.get('/celebrities', function (req, res, next) {
   Celebrity.find()
     .then(function(allcelebrities) {
@@ -14,5 +15,76 @@ router.get('/celebrities', function (req, res, next) {
     });
 })
 
+// ITERATION 4: Adding new celebrities
+router.get('/celebrities/new', function(req, res, next){
+  res.render('celebrities/new', {})
+})
+
+router.post('/celebrities', function(req, res, next){
+  const {name, occupation, catchPhrase} = req.body;
+
+  const newCelebrity = new Celebrity({name, occupation, catchPhrase})
+  newCelebrity.save()
+    .then(function(addedCelebrity){
+      res.redirect('/celebrities')
+    })
+    .catch(function(tryagain){
+      res.render('celebrities/new', {tryagain})
+    })
+})
+
+// ITERATION 5 : Deleting celebrities
+router.post('/celebrities/:id/delete', function(req, res, next){
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then((function(deletedCelebrity){
+      res.redirect('/celebrities')
+    }))
+    .catch(function (err) {
+      console.log(err);
+      next(err);
+    });
+})
+
+// ITERATION 6: Editing celebrities
+router.get('/celebrities/:id/edit', function(req, res, next){
+  Celebrity.findById(req.params.id)
+    .then(function (celebrityToUpdate){
+      res.render('celebrities/edit', {celebrityToUpdate})
+    })
+    .catch(function (err) {
+      console.log(err);
+      next(err);
+    });
+})
+
+router.post('/celebrities/:id', function(req, res, next){
+  const {name, occupation, catchPhrase} = req.body;
+  console.log(req.body)
+  Celebrity.findByIdAndUpdate(
+    req.params.id,
+    {name, occupation, catchPhrase},
+    { new: true} 
+    )
+    .then(function (updatedCelebrity){
+      console.log('test')
+      res.redirect(`celebrities/${updatedCelebrity.id}`)
+    })
+    .catch(function (err) {
+      console.log(err);
+      next(err);
+    });
+});
+
+// ITERATION 3 : Celebrity details page
+router.get('/celebrities/:id', function(req, res, next) {
+  Celebrity.findById(req.params.id)
+    .then(function (details) {
+      res.render('celebrities/show', {details});
+    })
+    .catch(function(err) {
+      console.log(err);
+      next(err);
+    })
+})
 
 module.exports = router;
