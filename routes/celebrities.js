e5442d77c3ad47e1d2efde313b47966385e8dff6
Lea Beaulieu <beaulieu.lea@free.fr>
2021-11-18 21:44:30 +0100

    done it 4

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -17,6 +17,31 @@ router.get("/celebrities", function(req, res, next) {
         });
 });
 
+router.get("/celebrities/new", function(req, res, next) {
+    res.render("celebrities/new", {
+
+    });
+})
+
+router.post("/celebrities/new", function(req, res, next) {
+    console.log("req.body=", req.body);
+
+    Celebrity.create({
+            name: req.body.name, // "tata"
+            occupation: req.body.occupation, // ""
+            catchPhrase: req.body.catchPhrase, // ""
+        })
+        .then(function(createdCelebrity) {
+            // celeb a été créé
+            res.redirect("/celebrities"); // reponse
+        })
+        .catch((err) => {
+            console.log(err);
+            next(err)
+                //res.redirect("/celebrities/new");;
+
+        })
+})
 
 router.get("/celebrities/:celebrityId", function(req, res, next) {
     Celebrity.findById(req.params.celebrityId)
@@ -34,82 +59,5 @@ router.get("/celebrities/:celebrityId", function(req, res, next) {
 });
 
 
-/*router.get("/books/:bookid/delete", function (req, res, next) {
-  Book.findByIdAndDelete(req.params.bookid)
-    .then(function () {
-      // livre a ete supp
-      res.redirect("/books");
-    })
-    .catch((err) => {
-      console.log(err);
-      // res.send('oops')
-      next(err);
-    });
-});
-
-router.get("/books/:bookid/edit", function (req, res, next) {
-  Book.findById(req.params.bookid)
-    .then(function (foundBook) {
-      res.render("book-edit", {
-        book: foundBook,
-      });
-    })
-    .catch(function (err) {
-      console.log(err);
-      next(err); // reponse
-    });
-});
-
-router.post("/books/:bookid/edit", function (req, res, next) {
-  Book.findByIdAndUpdate(
-    req.params.bookid,
-    {
-      title: req.body.title,
-      author: req.body.author,
-      rating: req.body.rating,
-      description: req.body.description,
-    },
-    { new: true }
-  )
-    .then(function (updatedBook) {
-      res.redirect("/books/" + req.params.bookid);
-    })
-    .catch((err) => console.log(err));
-});
-
-// FORM CREATION LIVRE
-router.get("/books/create", function (req, res, next) {
-  res.render("book-create", {});
-});
-
-//
-router.post("/books/create", function (req, res, next) {
-  console.log("req.bodyy=", req.body);
-
-  Book.create({
-    title: req.body.title, // "tata"
-    description: req.body.description, // ""
-    author: req.body.author, // ""
-    rating: Number(req.body.rating),
-  })
-    .then(function (createdBook) {
-      // livre a été créé
-      res.redirect("/books"); // reponse
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-// DETAIL D'UN LIVRE
-router.get("/books/:bookid", function (req, res, next) {
-  Book.findById(req.params.bookid) // "1234"
-    .then(function (bookDoc) {
-      res.render("book-details", {
-        mybook: bookDoc,
-      });
-    })
-    .catch((err) => console.log(err));
-});*/
 
 module.exports = router; // require('../book.routes.js)
