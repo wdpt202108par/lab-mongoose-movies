ca93e0993ddeab49c757e0edc57cfe58e76fea79
Yooyoo56 <tak.yoojeong@gmail.com>
2021-11-18 21:19:18 +0100

    iteration 5 finished

--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,71 @@
-const express = require('express');
-const router  = express.Router();
-const Celebrity = require('../models/celebrity');
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 /* GET celebrities page */
-//iteration 2 
-router.get('/celebrities', (req, res, next) =>{
+//iteration 2
+router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
-  .then(function (celebrityDB){
-    res.render('celebrities/index', {myCeleb: celebrityDB});
-    console.log('Connected to the port 3000😎');
-  })
-  .catch(function (err){
-    console.log('Error! During open the celebrtiy page');
-  })
-})
+    .then(function (celebrityDB) {
+      res.render("celebrities/index", { myCeleb: celebrityDB });
+      console.log("Connected to the port 3000😎");
+    })
+    .catch(function (err) {
+      console.log("Error! During open the celebrtiy page");
+    });
+});
 
 /* GET Adding New celebrities page */
 //  bon ordre!!!
 //iteration 4
-router.get('/celebrities/new', (req, res, next)=> {
-    res.render('celebrities/new' , {});
-})
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new", {});
+});
 
 //POST
-router.post("/celebrities", function (req, res, next){
-
-  console.log("req body is=", req.body);
-
+router.post("/celebrities", function (req, res, next) {
   Celebrity.create({
     name: req.body.name,
     occupation: req.body.occupation,
-    catchPhrase:req.body.catchPhrase,
+    catchPhrase: req.body.catchPhrase,
   })
-  .then(function (createdCelebrity){
-    res.redirect("/celebrities/new");
-  })
-  .catch((err)=>{
-    console.log('Error! creating new celebrities');
-  });
+    .then(function (createdCelebrity) {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log("Error! creating new celebrities");
+    });
 });
 
 /* GET celebrities id page */
 //iteration 3
-router.get("/celebrities/:id", (req, res, next)=> {
+router.get("/celebrities/:id", (req, res, next) => {
   Celebrity.findById(req.params.id)
-  .then(function (celebrityList){
-    res.render("celebrities/show", {myCeleb:celebrityList
+    .then(function (celebrityList) {
+      res.render("celebrities/show", { myCeleb: celebrityList });
+    })
+    .catch(function (err) {
+      console.log(`Error! ID celebrity page  not found🥵!`);
+      next(err);
     });
-  })
-  .catch(function (err){
-    console.log(`Error! ID celebrity page  not found🥵!`);
-    next(err);
-  })
-})
+});
 
-
+/* GET Deleting celebrities page */
+//iteration 5
+router.post("/celebrities/:id/delete", function (req, res, next) {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(function () {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log("Error! celebrity delete error!");
+      next(err);
+    });
+});
 
 module.exports = router;
