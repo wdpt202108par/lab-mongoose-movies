26c93fed55ab489014d6b99e984eed5b87c332ce
Kardelen Karatas <kardeln.karatas.ext@veolia.com>
2021-11-26 20:46:49 +0100

    iteration no4: new celebrity creation routes added

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,18 +1,53 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Celebrity = require('../models/celebrity.js')
 
 
 router.get('/celebrities', (req, res, next) => {
-  Celebrity.find({})
-      .then((allCelebrities) => {
-          console.log("alllll===>", allCelebrities)
-          res.render('celebrities/index', {
-              celebrities: allCelebrities
-          })
+    Celebrity.find({})
+        .then((allCelebrities) => {
+            res.render('celebrities/index', {
+                celebrities: allCelebrities
+            })
 
-      })
-      .catch(err => next(err))
+        })
+        .catch(err => next(err))
+})
+
+
+router.post('/celebrities', (req, res, next) => {
+
+    // const name = req.body.name
+    // const occupation = req.body.occupation
+    // const catchPhrase = req.body.catchPhrase 
+
+    const { name, occupation, catchPhrase } = req.body
+
+    // ici on peut aussi créer des condition pour le pas accepter des champs vide
+
+    // if(!name || !occupation || !catchPhrase){
+    //     res.render('celebrities/new', {message: "name, occupation, phrase fields are required"});
+    //     return
+    // }
+
+    Celebrity.create({
+            name,
+            occupation,
+            catchPhrase
+        })
+        .then((createdCelebrity) => {
+            console.log(`Celebrity created: ${createdCelebrity}`)
+            res.redirect('/celebrities')
+        })
+        .catch(err => {
+            console.log(`Error during creation: ${err}`)
+            res.render('celebrities/new');
+        })
+
+})
+
+router.get('/celebrities/new', (req, res, next) => {
+    res.render('celebrities/new')
 })
 
 
@@ -22,7 +57,6 @@ router.get('/celebrities/:id', (req, res, next) => {
             res.render('celebrities/show', chosenCelebrity)
         })
         .catch(err => next(err))
-  })
-  
+})
 
 module.exports = router;
